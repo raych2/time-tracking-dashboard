3eb1cf509275a7713757137e6f558dead7cc908a
raych2 <rtongco@gmail.com>
2023-05-24 21:03:02 -0700

    Fix hover color on ellipsis icon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60c06830bc320c81"; }
+/******/ 		__webpack_require__.h = function() { return "a6c87e9eaef0d2ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
