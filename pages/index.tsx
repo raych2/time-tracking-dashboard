c94ddd869d79e364976a1e96c5976e835cee18a2
raych2 <rtongco@gmail.com>
2023-05-28 14:51:15 -0700

    Add Head component to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import Head from "next/head";
 import { useState } from "react";
 import UserContainer from "../components/UserContainer";
 import ActivityContainer from "../components/ActivityContainer";
@@ -22,9 +23,22 @@ export default function Index() {
   if (!data) return <div>Loading...</div>;
 
   return (
-    <div className="reportContainer">
-      <UserContainer timeframe={timeframe} setTimeframe={setTimeframe} />
-      <ActivityContainer selectedTime={timeframe} data={data} />
-    </div>
+    <>
+      <Head>
+        <title>Frontend Mentor | Time tracking dashboard</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./images/favicon-32x32.png"
+        />
+      </Head>
+      <div className="reportContainer">
+        <UserContainer timeframe={timeframe} setTimeframe={setTimeframe} />
+        <ActivityContainer selectedTime={timeframe} data={data} />
+      </div>
+    </>
   );
 }
